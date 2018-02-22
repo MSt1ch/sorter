fe064f275ba8812ae40ea24104f4e91f1182bf1e
St1ch <st1ch@mail.ru>
2018-02-22 22:02:25 +0300

    part solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array = new Array();
+    this.compareNumber = (a, b) => a - b;
   }
 
   add(element) {
     // your implementation
+   return this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index]
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array
   }
 
   sort(indices) {
     // your implementation
+    var mass_sort = [];
+    for(var i=0; i < indices.length; i++){
+      mass_sort.push(this.array[indices[i]])
+    }
+
+    mass_sort.sort(this.compareNumber);
+    indices.sort(this.compareNumber);
+    
+    for(var i=0; i< indices.length; i++){
+      this.array[indices[i]] = mass_sort[i]
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareNumber = compareFunction;
   }
 }
 
